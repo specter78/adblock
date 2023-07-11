313a9833b183f80debea5e1cc008238d3d8f49e0
specter78 <40606342+specter78@users.noreply.github.com>
2023-07-11 13:25:24 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,16 +1,15 @@
 adblock = []
-discarded = []
 
 File.open("1.txt", "r") do |f|
   f.each_line do |line|
     line = line.strip
-    # if line.start_with?('||') && line.end_with?('.com^')
     if line.start_with?('!')
-        discarded << line
+    if line.start_with?('||') && line.end_with?('.com^')
+    if line.start_with?('||') && line.end_with?('.in^')
     else
       adblock << line
     end
   end
 end
 
-File.write("adblock.txt", discarded.join("\n"))
+File.write("adblock.txt", adblock.join("\n"))
