d3c45a2e84d5507a0c4ec02e9be046d977ebf26c
specter78 <40606342+specter78@users.noreply.github.com>
2023-07-11 11:09:39 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,1 +1,17 @@
+adblock = []
+discarded = []
 
+File.open("1.txt", "r") do |f|
+  f.each_line do |line|
+    if (line.match(/^\|\|.*\^$/))
+      discarded << line
+    else
+      adblock << line
+  end
+end
+
+final = adblock
+final << ["---", "---", "---"]
+final << discarded
+
+File.write("adblock.txt", final.join("\n"))
