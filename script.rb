b67943d88c2c805e4542bc94a9a51104e5a90a29
specter78 <40606342+specter78@users.noreply.github.com>
2023-07-11 13:23:43 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -3,6 +3,7 @@ discarded = []
 
 File.open("1.txt", "r") do |f|
   f.each_line do |line|
+    line = line.strip
     # if line.start_with?('||') && line.end_with?('.com^')
     if line.start_with?('!')
         discarded << line
