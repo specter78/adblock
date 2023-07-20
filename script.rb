f452192f3102e74e3a950a4e748deac43957a752
specter78 <40606342+specter78@users.noreply.github.com>
2023-07-20 18:49:49 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,12 +1,17 @@
-selected_rules = []
-discarded_rules = []
 blocklists = ['adguard_ads.txt', 'easylist.txt', 'adguard_privacy.txt', 'easyprivacy.txt']
 
 blocklists.each do |blocklist|
+  selected_rules = []
+  discarded_rules = []
+  skip_comments = false
   File.open(blocklist, "r") do |f|
     f.each_line do |line|
       line = line.strip
       if line.start_with?('! Checksum:')
+        # do nothing
+      elsif line.start_with?('!')
+        (skip_comments = true) if line.start_with?('!--')
+        (selected_rules << line) unless skip_comments
       elsif (line.count('/') == 0) && (line.start_with?('||')) && (line.end_with?('^') || line.end_with?('^$third-party'))
         discarded_rules << line
       else
@@ -15,6 +20,4 @@ blocklists.each do |blocklist|
     end
   end
   File.write(blocklist, selected_rules.join("\n"))
-  selected_rules = []
-  discarded_rules = []
 end
