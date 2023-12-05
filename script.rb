a3f6d1d43b36c3af9f4c000c9b6e0821469c4739
specter78 <40606342+specter78@users.noreply.github.com>
2023-12-05 15:32:45 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -3,7 +3,7 @@ require 'httparty'
 blocklists = ['adguard_ads.txt', 'easylist.txt', 'adguard_privacy.txt', 'easyprivacy.txt', 'adguard_mobile.txt']
 discarded_rules = []
 total_rules = 0
-dns_blocked = []
+$dns_blocked = []
 
 readme = []
 readme << "The script removes rules that can be blocked by DNS based ad-blocking.\n\n"
@@ -16,13 +16,13 @@ HTTParty.get('https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/mast
   next if url.start_with?('@@')
   url = url[2..-1] if url.start_with?('||')
   url = url[0..-2] if url.end_with?('^')
-  dns_blocked << url
+  $dns_blocked << url
 end
 
 def already_blocked?(url)
   url = url[2..-1] if url.start_with?('||')
   url = url.split('^')[0].split('/')[0]
-  return dns_blocked.include?(url)
+  return $dns_blocked.include?(url)
 end
 
 
