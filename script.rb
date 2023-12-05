44091d975bbf7ea2e0e1d8d44285a4e3e355b3a3
specter78 <40606342+specter78@users.noreply.github.com>
2023-12-05 13:57:22 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,5 +1,6 @@
 blocklists = ['adguard_ads.txt', 'easylist.txt', 'adguard_privacy.txt', 'easyprivacy.txt', 'adguard_mobile.txt']
 discarded_rules = []
+total_rules = 0
 readme = []
 readme << "The script removes rules that can be blocked by DNS based ad-blocking.\n\n"
 readme << "| File | Rules |"
@@ -33,6 +34,9 @@ blocklists.each do |blocklist|
   end
   File.write(blocklist, selected_rules.join("\n"))
   readme << "| #{blocklist} | #{selected_rules.count} |"
+  total_rules += selected_rules.count
 end
+
+readme << "| Total | #{total_rules.count} |"
 File.write("discarded.txt", discarded_rules.join("\n"))
 File.write("README.md", readme.join("\n"))
