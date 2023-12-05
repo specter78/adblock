acf2d780aeff5aa3ceab1195d10e8e25bc708268
specter78 <40606342+specter78@users.noreply.github.com>
2023-12-05 18:59:48 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,30 +1,5 @@
 require 'httparty'
 
-discarded_rules = []
-total_rules = 0
-$dns_blocked = []
-
-blocklists = []
-blocklists << ['https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt', 'adguard_ads.txt']
-blocklists << ['https://ublockorigin.github.io/uAssets/thirdparties/easylist.txt', 'easylist.txt']
-blocklists << ['https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt', 'adguard_privacy.txt']
-blocklists << ['https://ublockorigin.github.io/uAssets/thirdparties/easyprivacy.txt', 'easyprivacy.txt']
-blocklists << ['https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt', 'adguard_mobile.txt']
-
-blocklists.each do |list|
-  response = HTTParty.get(list[0])
-  if response.code == 200
-    File.write(list[1], response.body)
-  end
-end
-
-
-readme = []
-readme << "The script removes rules that can be blocked by DNS based ad-blocking.\n\n"
-readme << "| File | Rules |"
-readme << "|:----:|:-----:|"
-
-
 def adblock_format(blocklist)
   puts blocklist
   File.read(blocklist).each_line do |url|
@@ -44,7 +19,6 @@ def dns_format(blocklist)
 end
 
 def already_blocked?(url)
-  puts url
   if capture = /^(?:\|\|)?([a-zA-Z0-9\.,-]+).*/.match(url)
     return false if capture[1].include?(',')
     return $dns_blocked.include?(capture[1])
@@ -52,14 +26,24 @@ def already_blocked?(url)
   return false
 end
 
+
+discarded_rules = []
+total_rules = 0
+$dns_blocked = []
+readme = []
+readme << "The script removes rules that can be blocked by DNS based ad-blocking.\n\n"
+readme << "| File | Rules |"
+readme << "|:----:|:-----:|"
+
+
 published_list = []
-published_list = ['https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt', 'adguard_dns.txt', 'adp']
-published_list = ['https://big.oisd.nl', 'oisd.txt', 'adp']
-published_list = ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts', 'stevenblack.txt', 'dns']
-published_list = ['https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/pro.plus.txt', 'hagezi.txt', 'dns']
-published_list = ['https://hblock.molinero.dev/hosts', 'hblock.txt', 'dns']
-published_list = ['https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/hosts.txt', '1hosts.txt', 'dns']
-published_list = ['https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt', 'developerdan.txt', 'dns']
+published_list << ['https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt', 'adguard_dns.txt', 'adp']
+published_list << ['https://big.oisd.nl', 'oisd.txt', 'adp']
+published_list << ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts', 'stevenblack.txt', 'dns']
+published_list << ['https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/pro.plus.txt', 'hagezi.txt', 'dns']
+published_list << ['https://hblock.molinero.dev/hosts', 'hblock.txt', 'dns']
+published_list << ['https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/hosts.txt', '1hosts.txt', 'dns']
+published_list << ['https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt', 'developerdan.txt', 'dns']
 published_list.each do |list|
   response = HTTParty.get(list[0])
   if response.code == 200
@@ -68,23 +52,29 @@ published_list.each do |list|
   adblock_format(list[1]) if list[2] == 'adp'
   dns_format(list[1]) if list[2] == 'dns'
 end
-
-
 $dns_blocked = $dns_blocked.uniq
 
 
+blocklists = []
+blocklists << ['https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt', 'adguard_ads.txt']
+blocklists << ['https://ublockorigin.github.io/uAssets/thirdparties/easylist.txt', 'easylist.txt']
+blocklists << ['https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt', 'adguard_privacy.txt']
+blocklists << ['https://ublockorigin.github.io/uAssets/thirdparties/easyprivacy.txt', 'easyprivacy.txt']
+blocklists << ['https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt', 'adguard_mobile.txt']
+
 blocklists.each do |list|
   selected_rules = []
-  File.open(list[1], "r") do |f|
-    f.each_line do |line|
-      line = line.strip
-      if line.start_with?('!')
-      elsif line == ''
-      elsif already_blocked?(line)
-        discarded_rules << line
-      else
-        selected_rules << line
-      end
+  response = HTTParty.get(list[0])
+  next if response.code != 200
+  response.body.each_line do |line|
+    if line.start_with?('!')
+    elsif line == ''
+    elsif '.#/-@$%&*'.include?(line[0])
+      selected_rules << line
+    elsif already_blocked?(line)
+      discarded_rules << line
+    else
+      selected_rules << line
     end
   end
   
