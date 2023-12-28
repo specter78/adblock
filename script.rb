f5358047e764743f3880ff6c2e31e9143f3234bc
specter78 <40606342+specter78@users.noreply.github.com>
2023-12-28 23:47:50 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -17,15 +17,11 @@ def dns_format(blocklist)
 end
 
 def already_blocked?(url)
-  if capture = /^(?:@@)?(?:\|\|)?([a-zA-Z0-9\.,\-_]+[a-zA-Z0-9])\^.*/.match(url)
+  if capture = /^(?:@@)?(?:\|\|)?([a-zA-Z0-9\.,\-_]+[a-zA-Z0-9])(?:\^|#).*/.match(url)
     return false if capture[1].include?(',')
     return true unless capture[1].ascii_only?
     domain = capture[1]
     return true if $dns_blocked[domain]
-    # while domain.index('.') != nil
-    #   return true if $dns_blocked[domain]
-    #   domain = domain[(domain.index('.')+1)..-1]
-    # end
   end
   return false
 end
