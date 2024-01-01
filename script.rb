90315db5ffc213ff91acac79be806cc48f3d0c34
specter78 <40606342+specter78@users.noreply.github.com>
2024-01-01 11:58:48 +0530

    Refactor

--- script.rb
+++ script.rb
@@ -78,14 +78,13 @@ end
 
 
 published_list = []
-published_list << ['https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt', 'filters/adguard_dns.txt', 'abp']
-published_list << ['https://raw.githubusercontent.com/sjhgvr/oisd/main/domainswild_big.txt', 'filters/oisd.txt', 'domain']
-published_list << ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts', 'filters/stevenblack.txt', 'host']
-published_list << ['https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus.txt', 'filters/hagezi.txt', 'domain']
-published_list << ['https://hblock.molinero.dev/hosts_domains.txt', 'filters/hblock.txt', 'domain']
-# published_list << ['https://o0.pages.dev/Lite/domains.wildcards', 'filters/1hosts.txt', 'domain']
-published_list << ['https://o0.pages.dev/Pro/domains.wildcards', 'filters/1hosts.txt', 'domain']
-published_list << ['https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt', 'filters/developerdan.txt', 'host']
+published_list << ['https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt', 'dns/adguard_dns.txt', 'abp']
+published_list << ['https://raw.githubusercontent.com/sjhgvr/oisd/main/domainswild_big.txt', 'dns/oisd.txt', 'domain']
+published_list << ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts', 'dns/stevenblack.txt', 'host']
+published_list << ['https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus.txt', 'dns/hagezi.txt', 'domain']
+published_list << ['https://hblock.molinero.dev/hosts_domains.txt', 'dns/hblock.txt', 'domain']
+published_list << ['https://o0.pages.dev/Pro/domains.wildcards', 'dns/1hosts.txt', 'domain']
+published_list << ['https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt', 'dns/developerdan.txt', 'host']
 published_list.each do |list|
   begin
     response = HTTParty.get(list[0])
@@ -99,32 +98,45 @@ end
 
 
 blocklists = []
-blocklists << ['https://ublockorigin.github.io/uAssets/thirdparties/easyprivacy.txt', 'easyprivacy.txt']
-# uBlock Origin compatible
-blocklists << ['https://filters.adtidy.org/extension/ublock/filters/2.txt', 'adguard_ads_+_easylist_(ublock).txt']
-blocklists << ['https://filters.adtidy.org/extension/ublock/filters/3.txt', 'adguard_privacy_(ublock).txt']
-blocklists << ['https://filters.adtidy.org/extension/ublock/filters/11.txt', 'adguard_mobile_(ublock).txt']
-blocklists << ['https://filters.adtidy.org/extension/ublock/filters/14.txt', 'adguard_annoyances_(ublock).txt']
-blocklists << ['https://filters.adtidy.org/extension/ublock/filters/4.txt', 'adguard_social_(ublock).txt']
+blocklists << ['https://easylist.to/easylist/easyprivacy.txt', 'easylist/easyprivacy.txt']
+# uBlock compatible
+blocklists << ['https://filters.adtidy.org/extension/ublock/filters/2.txt', 'ublock/adguard_base.txt'] # AdGuard English + EasyList
+blocklists << ['https://filters.adtidy.org/extension/ublock/filters/3.txt', 'ublock/adguard_tracking_protection.txt']
+blocklists << ['https://filters.adtidy.org/extension/ublock/filters/4.txt', 'ublock/adguard_social.txt']
+blocklists << ['https://filters.adtidy.org/extension/ublock/filters/11.txt', 'ublock/adguard_mobile.txt']
+blocklists << ['https://filters.adtidy.org/extension/ublock/filters/14.txt', 'ublock/adguard_annoyances.txt']
+blocklists << ['https://filters.adtidy.org/extension/ublock/filters/17.txt', 'ublock/adguard_url_tracking.txt']
 # AdGuard compatible
-blocklists << ['https://filters.adtidy.org/mac_v2/filters/2.txt', 'adguard_ads_+_easylist_(adguard).txt']
-blocklists << ['https://filters.adtidy.org/mac_v2/filters/3.txt', 'adguard_privacy_(adguard).txt']
-blocklists << ['https://filters.adtidy.org/mac_v2/filters/11.txt', 'adguard_mobile_(adguard).txt']
-blocklists << ['https://filters.adtidy.org/mac_v2/filters/14.txt', 'adguard_annoyances_(adguard).txt']
-blocklists << ['https://filters.adtidy.org/mac_v2/filters/4.txt', 'adguard_social_(adguard).txt']
-# AdGuard optimized
-blocklists << ['https://filters.adtidy.org/extension/safari/filters/2_optimized.txt', 'adguard_ads_+_easylist_(safari).txt']
+blocklists << ['https://filters.adtidy.org/mac_v2/filters/2.txt', 'adguard/adguard_base.txt'] # AdGuard English + EasyList
+blocklists << ['https://filters.adtidy.org/mac_v2/filters/3.txt', 'adguard/adguard_tracking_protection.txt']
+blocklists << ['https://filters.adtidy.org/mac_v2/filters/4.txt', 'adguard/adguard_social.txt']
+blocklists << ['https://filters.adtidy.org/mac_v2/filters/11.txt', 'adguard/adguard_mobile.txt']
+blocklists << ['https://filters.adtidy.org/mac_v2/filters/14.txt', 'adguard/adguard_annoyances.txt']
+blocklists << ['https://filters.adtidy.org/mac_v2/filters/17.txt', 'adguard/adguard_url_tracking.txt']
+# AdGuard Safari optimized
+blocklists << ['https://filters.adtidy.org/extension/safari/filters/2_optimized.txt', 'safari/adguard_base.txt'] # AdGuard English + EasyList
+blocklists << ['https://filters.adtidy.org/extension/safari/filters/3_optimized.txt', 'safari/adguard_tracking_protection.txt']
+blocklists << ['https://filters.adtidy.org/extension/safari/filters/4_optimized.txt', 'safari/adguard_social.txt']
+blocklists << ['https://filters.adtidy.org/extension/safari/filters/11_optimized.txt', 'safari/adguard_mobile.txt']
+blocklists << ['https://filters.adtidy.org/extension/safari/filters/14_optimized.txt', 'safari/adguard_annoyances.txt']
+# AdGuard iOS optimized
+blocklists << ['https://filters.adtidy.org/ios/filters/2_optimized.txt', 'ios/adguard_base.txt'] # AdGuard English + EasyList
+blocklists << ['https://filters.adtidy.org/ios/filters/3_optimized.txt', 'ios/adguard_tracking_protection.txt']
+blocklists << ['https://filters.adtidy.org/ios/filters/4_optimized.txt', 'ios/adguard_social.txt']
+blocklists << ['https://filters.adtidy.org/ios/filters/11_optimized.txt', 'ios/adguard_mobile.txt']
+blocklists << ['https://filters.adtidy.org/ios/filters/14_optimized.txt', 'ios/adguard_annoyances.txt']
+
 
 blocklists.each do |list|
-  original_rules = 0
-  selected_rules = ["! Title: #{list[1].split('.')[0].split('_').collect{|x| x.capitalize}.join(" ")} Modified"]
+  original_rules_count = 0
+  selected_rules = ["! Title: #{list[1].split('.')[0].split('/')[1].split('_').collect{|x| x.capitalize}.join(" ")} Modified"]
   selected_rules << ["! TimeUpdated: #{DateTime.now.new_offset(0).to_s}"]
   selected_rules << ['! Expires: 6 hours (update frequency)']
   selected_rules << ['! Homepage: https://github.com/specter78/adblock']
   response = HTTParty.get(list[0])
   next if response.code != 200
   response.body.each_line do |line|
-    original_rules += 1
+    original_rules_count += 1
     line = line.strip
     if line.start_with?('!')
     elsif line == ''
@@ -140,8 +152,7 @@ blocklists.each do |list|
   end
   
   File.write(list[1], selected_rules.join("\n"))
-  readme << "| #{list[1]} | #{original_rules.count} | #{selected_rules.count} |"
+  readme << "| #{list[1].split('.')[0]} | #{original_rules_count} | #{selected_rules.count} |"
 end
 
-# File.write("discarded.txt", discarded_rules.join("\n"))
 File.write("README.md", readme.join("\n"))
