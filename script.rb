a59b04ac9eaaf08557ba92d22687ed4282cde95d
specter78 <40606342+specter78@users.noreply.github.com>
2024-01-03 19:16:05 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,23 +1,22 @@
 require 'httparty'
 
-def already_blocked?(url)
-  if capture = /^(?:@@)?(?:\|\|)?([^#^\^^$^%]+)(.*)/.match(url)
+def already_blocked?(line)
+  if capture = /^(?:@@)?(?:\|\|)?([^#^\^^$^%]+)(.*)/.match(line)
     return if capture[1].include?(',')
     return unless capture[1].ascii_only?
-    capture[1].split('/')[0].include?('.') ? (domain = capture[1].split('/')[0].split(':')[0]) : (domain = capture[1])
-    return if domain.include?('*')
-    return if domain.include?('~')
-    return if domain[-1] == '.'
-    return if domain[-1] == '-'
-    return if domain[-1] == '_'
-    puts domain
-    $domain_rules[domain] += 1
-    $tld_rules[domain.split('.').last] += 1
-    # $domain_rules[domain.split('.')[-2..-1].join('.')] += 1
-    # while domain.index('.') != nil
-    #   return true if $dns_blocked[domain]
-    #   domain = domain[(domain.index('.')+1)..-1]
-    # end
+    return if capture[1][-1] == '.'
+    return if capture[1][-1] == '-'
+    return if capture[1][-1] == '_'
+    return if capture[1][0] == '~'
+    if capture[1].split('/')[0].include?('.')
+      domain = domain.split('.')[-2..-1].join('.')
+      tld = domain.split('.').last
+      $domain_rules[domain] += 1 unless domain.include?('*')
+      $tld_rules[tld] += 1
+    else
+      tld = domain.split('.').last
+      $tld_rules[tld] += 1
+    end
   end
 end
 
@@ -35,16 +34,17 @@ end
 
 $domain_rules = Hash.new(0)
 $tld_rules = Hash.new(0)
+
 blocklists = []
-blocklists << ['https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_social_optimized.txt']
-blocklists << ['https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_annoyances_optimized.txt']
-# blocklists << ['https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_base.txt']
-# blocklists << ['https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_mobile.txt']
-# blocklists << ['https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_tracking_protection.txt']
-# blocklists << ['https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_url_tracking.txt']
+blocklists << 'https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_annoyances_optimized.txt'
+blocklists << 'https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_base_optimized.txt'
+blocklists << 'https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_mobile_optimized.txt'
+blocklists << 'https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_social_optimized.txt'
+blocklists << 'https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_tracking_protection_optimized.txt'
+blocklists << 'https://raw.githubusercontent.com/specter78/adblock/main/ublock/adguard_url_tracking_optimized.txt'
 
 blocklists.each do |list|
-  response = HTTParty.get(list[0])
+  response = HTTParty.get(list)
   next if response.code != 200
   response.body.each_line do |line|
     line = line.strip
@@ -59,7 +59,7 @@ readme = []
 readme << "Domain Counter\n\n"
 readme << "| Domain | Rules |"
 readme << "|:----:|:-----:|"
-$domain_rules.delete_if {|k,v| v < 100 }.to_a.sort_by{ |x| x[1] }.reverse.each{ |x| readme << "| #{x[0]} | #{x[1]} |" }
+$domain_rules.delete_if {|k,v| v < 50 }.to_a.sort_by{ |x| x[1] }.reverse.each{ |x| readme << "| #{x[0]} | #{x[1]} |" }
 readme << "\n\nTLD Counter\n\n"
 readme << "| TLD | Rules |"
 readme << "|:----:|:-----:|"
