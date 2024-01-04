92adab93fb6f34b8e4372840c7eea90196972f76
specter78 <40606342+specter78@users.noreply.github.com>
2024-01-04 23:41:13 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -33,11 +33,11 @@ def already_blocked?(line, filename)
     return false if domain[-1] == '.'
     return false if domain[0] == '~'
 
-    if /^(?:ios|safari).*optimized/.match(filename) # ios filter list optimization
+    if /^(?:ios|safari).*optimized/.match(filename) # ios/safari filter list optimization
       return true if /^(.*\.)?google\./.match(domain) && (not /\.(com|in|\*)$/.match(domain)) # google in all files
       if /(?:annoyances|social)/.match(filename)
         return true if /^(.*\.)?yandex\./.match(domain) # yandex in annoyances and social
-        return true if /\.(?:ru|de|jp|pl|ua|tr|it|fr|br|nl|by|es|kr|cn|cz|hu|no|ch)$/.match(domain) # tlds in annoyances and social
+        return true if /\.(?:ru|de|jp|pl|ua|tr|it|fr|br|nl|by|es|kr|cn|cz|hu|no|ch|at|tw|uk)$/.match(domain) # tlds in annoyances and social
       end
     end
     
