40a9cbed2d942008d7a671a63151652ff5716f03
specter78 <40606342+specter78@users.noreply.github.com>
2024-01-04 12:47:06 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -33,7 +33,7 @@ def already_blocked?(line)
     if capture[1].index('.') && capture[1].index('/') && (capture[1].index('.') > capture[1].index('/'))
       puts capture[1]
       temp_capture = /^(?:\|)?(?:https?\*?)?(?:\:\/\/\/?)?(.*)/.match(capture[1])
-      puts capture[1] + ' --> ' + temp_capture[1] if (temp_capture[1].index('.') > temp_capture[1].index('/'))
+      puts capture[1] + ' --> ' + temp_capture[1] if temp_capture[1].index('.') && temp_capture[1].index('/') && (temp_capture[1].index('.') > temp_capture[1].index('/'))
     end
     capture[1].split('/')[0].include?('.') ? (domain = capture[1].split('/')[0].split(':')[0]) : (domain = capture[1])
     return false if domain[-1] == '.'
