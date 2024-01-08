3a67d27cca0db919d16e1d19c503a0673ec5e26d
specter78 <40606342+specter78@users.noreply.github.com>
2024-01-08 10:50:03 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -34,12 +34,16 @@ def already_blocked?(domain, line, filename)
     return false if domain[0] == '~'
     
     if /optimized/.match(filename) # filter list optimization
+
+      return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'pagead2.googlesyndication.com')")
+      return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')")
+      return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')")
+      return true if line.end_with?("#%#//scriptlet('prevent-xhr', 'pagead2.googlesyndication.com')")
+      
       return true if /(?:facebook\.com|facebook\.net|instagram\.com|onion)$/.match(domain) # selected domains in all files
       return true if /^(.*\.)?yandex\./.match(domain) && line.include?('#') # yandex in all files
       return true if /^(.*\.)?google\./.match(domain) && (not /\.(com|in|\*)$/.match(domain)) # !com and !in google in all files
       return true if /(?:#@?%#|#@?\?#|#@?\$\?#)/.match(line) && /\.(?:pl|jp|ru|de|fr|es)$/.match(domain) # advanced/extended rules for selected tlds
-      return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'pagead2.googlesyndication.com')")
-      return true if line.end_with?("#%#//scriptlet('prevent-xhr', 'pagead2.googlesyndication.com')")
       return true if /^e?mail\..*\$image$/.match(line)
       if /(?:annoyances|social)/.match(filename)
         return true if (line.start_with?('||') || line.include?('#') || line.include?('domain=')) && domain.include?('.') && (not /\.(?:com|in|org|to|tv|\*)$/.match(domain)) # tlds in annoyances and social
