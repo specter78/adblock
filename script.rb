9adca5bc224862af67f0c608bcad18b26ccbd173
specter78 <40606342+specter78@users.noreply.github.com>
2024-01-08 11:54:36 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -35,13 +35,8 @@ def already_blocked?(domain, line, filename)
     
     if /optimized/.match(filename) # filter list optimization
 
-      # return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'pagead2.googlesyndication.com')")
-      # return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')")
-      # return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')")
-      # return true if line.end_with?("#%#//scriptlet('prevent-xhr', 'pagead2.googlesyndication.com')")
       return true if /#%#\/\/scriptlet\('prevent-(?:fetch|xhr)', '(?:https:\/\/)?pagead2\.googlesyndication\.com(?:\/pagead\/js\/adsbygoogle\.js)?'\)$/.match(line)
-      return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'www3.doubleclick.net')")
-      return true if line.end_with?("#%#//scriptlet('prevent-fetch', 'doubleclick.net')")
+      return true if /#%#\/\/scriptlet\('prevent-(?:fetch|xhr)', '(?:www3\.)?doubleclick\.net'\)$/.match(line)
       
       return true if /(?:facebook\.com|facebook\.net|instagram\.com|onion)$/.match(domain) # selected domains in all files
       return true if /^(.*\.)?yandex\./.match(domain) && line.include?('#') # yandex in all files
