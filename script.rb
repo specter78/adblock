a8a021d2e3ae886db1d8e4ad181df6c95bb6628e
specter78 <40606342+specter78@users.noreply.github.com>
2024-03-24 00:07:46 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -58,9 +58,9 @@ end
 
 def optimize_rule(line, filename)
 
-  if capture = /#%#\/\/scriptlet\(['"]prevent-(?:fetch|xhr)['"], ['"]([^'^"^|^\)]+)['"]\)$/.match(line)
-    return "" if already_blocked?(capture[1], line, filename)
-  end
+  # if capture = /#%#\/\/scriptlet\(['"]prevent-(?:fetch|xhr)['"], ['"]([^'^"^|^\)]+)['"]\)$/.match(line)
+  #   return "" if already_blocked?(capture[1], line, filename)
+  # end
   # if capture = /##\+js\(no-(?:fetch|xhr)-if, ([^|^\)]+)\)$/.match(line)
   #   return "" if already_blocked?(capture[1], line, filename)
   # end
