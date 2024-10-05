212ad9a3fd4176701112694c43d547ae5fb5e8b2
specter78 <40606342+specter78@users.noreply.github.com>
2024-10-06 03:00:57 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -30,8 +30,7 @@ def already_blocked?(domain, line, platform, filename)
     return true unless capture[1].ascii_only?
     return false if capture[1].index('.') && capture[1].index('/') && (capture[1].index('/') < capture[1].index('.'))
     domain = capture[1].split('/')[0]
-    return false if domain[0] == '['
-    return false if domain[0] == '/'
+    return false if domain.nil?
     return false if domain[-1] == '.'
     return false if domain[0] == '~'
     
