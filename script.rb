7c00d0c0b6148f7247032be6a3e311826ff2eddd
specter78 <40606342+specter78@users.noreply.github.com>
2024-10-06 02:57:20 +0530

    Update script.rb

--- script.rb
+++ script.rb
@@ -30,9 +30,9 @@ def already_blocked?(domain, line, platform, filename)
     return true unless capture[1].ascii_only?
     return false if capture[1].index('.') && capture[1].index('/') && (capture[1].index('/') < capture[1].index('.'))
     domain = capture[1].split('/')[0]
+    return false if domain[0] == '['
     return false if domain[-1] == '.'
     return false if domain[0] == '~'
-    return false if domain[0] == '['
     
     # filter list optimization      
     return true if /^(.*\.)?(?:facebook\.com|facebook\.net|fb\.com|onion)$/.match(domain) # selected domains in all files
